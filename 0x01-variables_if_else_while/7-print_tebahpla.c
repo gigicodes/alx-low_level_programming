57626b8e6b23dfd66ac024ae841cabd3a9a40689
Giftie <gifftchiedozie@gmail.com>
2022-04-10 11:28:02 -0700

    C program #7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabet in reverse
+ *
+ * Return: Always 0 (Success/Correct)
+ */
+int main(void)
+
+{
+
+char r_alphabet;
+
+for (r_alphabet = 122; r_alphabet >= 97; r_alphabet++)
+{
+putchar(r_alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
