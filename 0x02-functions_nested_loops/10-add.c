696bb92b6b2c689cdebfdb7341684b37b822ce76
Giftie <gifftchiedozie@gmail.com>
2022-04-12 21:04:21 -0700

    C Program #10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+i#include "main.h"
+#include <stdio.h>
+/**
+ * add - function that adds two integers and shows result
+ * @x: first inputted parameter
+ * @y: second inputted parameter
+ *
+ * Return: Always 0 (Correct)
+ */
+int add(int x, int y)
+{
+return (x + y);
+}
