1b64c2b07887f46a365f5b292e4ce695b88d8918
Giftie <gifftchiedozie@gmail.com>
2022-04-15 19:53:35 -0700

    C Program #101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,14 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * * print_number - that prints an integer
- * *@n: print int
- * * Return: Always 0.
- * */
+ * print_number - that prints an integer
+ * @n: print int
+ * Return: Always 0.
+ */
 void print_number(int n)
 {
-	int n;
 	printf("Enter any number: ");
-	scanf("%d", n);
+	scanf("%d", &n);
 	_putchar('\n');
-	return (0);
 }
